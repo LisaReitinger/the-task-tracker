38c3ca18cb40aac2e87133f144f2d025853ac4ab
Lisa Reitinger <lisareitinger7@gmail.com>
2025-05-27 15:33:46 +0200

    Add env.py for environment variable configuration

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["IP"] = "0.0.0.0"
+os.environ["PORT"] = "5000"
+os.environ["SECRET_KEY"] = "your_secret_key_here"
+os.environ["DEBUG"] = "True"
+os.environ["DEVELOPMENT"] = "True"
+os.environ["DB_URL"] = "postgresql://localhost:5432/taskmanager"
